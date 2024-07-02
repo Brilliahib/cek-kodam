31a917ee5b81a7303f747b28d3a440f98ccb7db9
brilliahib <brilliahib21@gmail.com>
2024-07-02 13:52:26 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,82 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
+import PatungUndip from "@/public/img/patung-undip.png";
+import { ListKhodam } from "@/lib/constant";
+import { useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const [result, setResult] = useState<{
+    name: string;
+    khodam: string;
+    desc: string;
+  } | null>(null);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!name) {
+      setResult({ name: "", khodam: "Isi nama dulu lur!", desc: "" });
+      return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * ListKhodam.length);
+    console.log(randomIndex);
+
+    const randomKhodam = ListKhodam[randomIndex];
+
+    setResult({ name, khodam: randomKhodam.name, desc: randomKhodam.desc });
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="pad-x flex min-h-screen items-center justify-center">
+      <div className="space-y-4 w-full">
+        <div className="text-center space-y-2">
+          <h1 className="md:text-5xl text-4xl font-extrabold">Cek Kodam</h1>
+          <h1 className="md:text-5xl text-4xl font-extrabold text-purple-400">
+            Anak Tembalang
+          </h1>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="w-full">
+          <div className="flex justify-center">
+            <Image
+              src={PatungUndip}
+              alt="Patung Undip"
+              className="max-w-[250px]"
+              width={364}
+              height={463}
+            ></Image>
+          </div>
+          <div className="text-center space-y-1 mt-8 w-full">
+            {result && (
+              <>
+                <h1 className="text-xl font-extrabold underline mb-2">
+                  {result.name}
+                </h1>
+                <h3 className="text-xl font-extrabold mb-4">{result.khodam}</h3>
+                <div className="text-md font-semibold">
+                  <p className="md:max-w-sm">{result.desc}</p>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+        <div>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <Input
+              type="text"
+              placeholder="Enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Input>
+            <Button
+              type="submit"
+              className="bg-violet-700 w-full font-extrabold text-lg hover:bg-violet-700/60"
+            >
+              Cek Kodam
+            </Button>
+          </form>
+        </div>
       </div>
     </main>
   );
---
+++ lib/constant.ts
@@ -0,0 +1,47 @@
+export const ListKhodam = [
+  {
+    id: 1,
+    name: "Es Teh Jumbo",
+    desc: "Es Teh Jumbo, saksi bisu bahwa tembalang lebih panas dari hubungan LDR.",
+  },
+  {
+    id: 2,
+    name: "Sapi FPP",
+    desc: "Patung Sapi FPP yang konon katanya waktu malam bisa jalan sendiri.",
+  },
+  {
+    id: 3,
+    name: "Patung Kuda Pangeran Diponegoro",
+    desc: "Spot favorit foto dan bikin jj anak mahasiswa baru.",
+  },
+  {
+    id: 4,
+    name: "Beton Gondang",
+    desc: "Beton yang sering bikin emosi di kerumunan jalan. ",
+  },
+  {
+    id: 5,
+    name: "Takoyaki Indomaret Undip",
+    desc: "Takoyaki 1000an yang isinya kayak htsmu, kadang ada kadang nggak. Btw, HTS-mu sekarang kemana?",
+  },
+  {
+    id: 6,
+    name: "Jasuke Slowmo",
+    desc: "Jasuke yang pelayanannya udah kayak nunggu doi bales chat. ",
+  },
+  {
+    id: 7,
+    name: "Marlin FPIK",
+    desc: "Ikan Nabi Adam yang sempat viral di Tiktok. Info spot mancing yang isinya ikan nabi adam lur?",
+  },
+  {
+    id: 8,
+    name: "Omelet Bakar Bu Nur",
+    desc: "Omelet bakar yang rasanya tidak bisa tergantikan. Mantanmu gabisa tergantikan ya?",
+  },
+  {
+    id: 9,
+    name: "Pancong Lumer",
+    desc: "Makanan favorit mahasiswa baru dan mahasiswa tua. Untuk mahasiswa tua jangan lupa kerjain skripsimu hehehe.",
+  },
+];
